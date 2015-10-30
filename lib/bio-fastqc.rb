51974af64c4a6870ae7e16310541662a1447f02f
Tazro Inutano Ohta <inutano@gmail.com>
2015-10-30 09:35:30 +0900

    location of components fixed

--- lib/bio-fastqc.rb
+++ lib/bio-fastqc.rb
@@ -2,11 +2,12 @@
 # bioruby directory tree.
 #
 # For example, say you have a plugin named bio-plugin, the only uncommented
-# line in this file would be 
+# line in this file would be
 #
 #   require 'bio/bio-plugin/plugin'
 #
 # In this file only require other files. Avoid other source code.
 
 require 'bio/fastqc/cli'
+require 'bio/fastqc/parser'
 require 'bio/fastqc'
--- lib/bio/fastqc/cli.rb
+++ lib/bio/fastqc/cli.rb
@@ -2,11 +2,13 @@
 
 require 'thor'
 
-module BioFastqc
-  class CLI < Thor
-    desc "parse fastqc result", "parse fastqc_data.txt in DIR or ZIPFILE from fastqc command."
-    def parse(file)
-      puts "parsed :D"
+module Bio
+  module FastQC
+    class CLI < Thor
+      desc "parse fastqc result", "parse fastqc_data.txt in DIR or ZIPFILE from fastqc command."
+      def parse(file)
+        puts "parsed :D"
+      end
     end
   end
 end
