5be42be467c94ca396d71f43087e352782d1a651
Tazro Inutano Ohta <inutano@gmail.com>
2015-11-06 11:40:44 +0900

    multiple input files, error handling

--- lib/bio/fastqc/cli.rb
+++ lib/bio/fastqc/cli.rb
@@ -6,9 +6,13 @@ require 'json'
 module Bio
   module FastQC
     class CLI < Thor
-      desc "parse [filename]", "parse fastqc data in fastqc directory or zipfile"
-      def parse(file)
-        puts JSON.dump(Parser.new(Data.read(file)).summary)
+      desc "parse [filename]...", "parse fastqc data in fastqc directory or zipfile, output in json format"
+      def parse(*files)
+        files.each do |file|
+          puts JSON.dump(Parser.new(Data.read(file)).summary)
+        end
+      rescue
+        puts "Wrong input file type: specify fastqc result data, directory or zipfile"
       end
     end
   end
