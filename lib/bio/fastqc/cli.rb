323381cf79ec7d8b7acd92d613986ef3a934494d
Tazro Inutano Ohta <inutano@gmail.com>
2016-03-15 16:58:54 +0900

    parse command accepts only one filename; iterate for multiple files

--- lib/bio/fastqc/cli.rb
+++ lib/bio/fastqc/cli.rb
@@ -6,11 +6,11 @@ require 'json'
 module Bio
   module FastQC
     class CLI < Thor
-      desc "parse [filename]...", "parse fastqc data in fastqc directory or zipfile, output in json format"
-      def parse(*files)
-        files.each do |file|
-          puts JSON.dump(Parser.new(Data.read(file)).summary)
-        end
+      desc "parse [filename]", "parse fastqc data in fastqc directory or zipfile, output in json format"
+      def parse(file)
+        data = Data.read(file)
+        summary = Parser.new(data).summary
+        puts JSON.dump(summary)
       rescue
         puts "Wrong input file type: specify fastqc result data, directory or zipfile"
       end
