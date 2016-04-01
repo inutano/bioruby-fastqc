ac98dc8592a8a584dd538a08d9384d44974c002c
Tazro Inutano Ohta <inutano@gmail.com>
2016-04-01 18:03:51 +0900

    fully refactored

--- lib/bio/fastqc/parser.rb
+++ lib/bio/fastqc/parser.rb
@@ -5,249 +5,250 @@ module Bio
     class Parser
       def initialize(fastqc_data_txt)
         @data = fastqc_data_txt
-        @object = parse(@data)
-        @base = self.basic_statistics
+        @module_results = parse_modules
+        @basic_statistics = basic_statistics
       end
 
-      def parse(data)
-        modules = data.split(">>END_MODULE\n")
-        modules.map do |node|
-          lines = node.split("\n")
-          rm_header = lines.map do |line|
-            if line !~ /^\#/ || line =~ /^#Total Duplicate Percentage/
-              line.split("\t")
-            end
-          end
-          rm_header.compact
+      def parse_modules
+        @data.split(">>END_MODULE\n").map do |mod|
+          mod.split("\n").map{|line| line.split("\t") }
         end
       end
 
-      def fastqc_version
-        @data.split("\n").first.split("\t").last
-      end
+      #
+      # Basic Statistics module
+      #
 
       def basic_statistics
-        Hash[*@object.select{|a| a.first.first == ">>Basic Statistics" }.flatten]
+        Hash[*@module_results[0].flatten]
       end
 
-      def filename
-        @base["Filename"]
+      def fastqc_version # software version of FastQC
+        @basic_statistics["##FastQC"]
       end
 
-      def file_type
-        @base["File type"]
+      def filename # input filename for FastQC program
+        @basic_statistics["Filename"]
       end
 
-      def encoding
-        @base["Encoding"]
+      def file_type # input file type
+        @basic_statistics["File type"]
       end
 
-      def total_sequences
-        @base["Total Sequences"].to_i
+      def encoding # quality encoding method for input file type
+        @basic_statistics["Encoding"]
       end
 
-      def filtered_sequences
-        @base["Filtered Sequences"].to_i
+      def total_sequences # total number of sequence reads
+        @basic_statistics["Total Sequences"].to_i
       end
 
-      def sequence_length
-        @base["Sequence length"]
+      def sequences_flagged_as_poor_quality # number of sequence reads flagged as poor quality
+        @basic_statistics["Sequences flagged as poor quality"].to_i
       end
 
-      def min_length
-        l = @base["Sequence length"]
-        if l =~ /\d-\d/
-          l.sub(/-\d+$/,"").to_i
-        else
-          l.to_i
+      def filtered_sequences # number of sequence reads filtered out
+        @basic_statistics["Filtered Sequences"].to_i
+      end
+
+      def sequence_length # store as string: can be range
+        @basic_statistics["Sequence length"]
+      end
+
+      def percent_gc # overall percentage of GC content
+        @basic_statistics["%GC"].to_f
+      end
+
+      #
+      # Other modules
+      #
+
+      def get_module_matrix(module_name, num_of_header_rows)
+        mod = @module_results.select{|m| m[0][0] == ">>#{module_name}" }[0]
+        if mod
+          mod.shift(num_of_header_rows)
+          mod
         end
-      end
-
-      def max_length
-        l = @base["Sequence length"]
-        if l =~ /\d-\d/
-          l.sub(/^\d+-/,"").to_i
-        else
-          l.to_i
-        end
-      end
-
-      def percent_gc
-        @base["%GC"].to_i
       end
 
       def per_base_sequence_quality
-        node = @object.select{|a| a.first.first == ">>Per base sequence quality" }.first
-        node.select{|n| n.first != ">>Per base sequence quality" } if node
-      end
-
-      ## Custom module: overall mean base call quality indicator
-      def overall_mean_quality_score
-        per_base = self.per_base_sequence_quality
-        if per_base
-          v = per_base.map{|c| (10**(c[1].to_f/-10)).to_f }
-          -10 * Math.log10(v.reduce(:+) / v.size)
-        end
-      end
-
-      ## Custom module: overall median base call quality indicator
-      def overall_median_quality_score
-        per_base = self.per_base_sequence_quality
-        if per_base
-          v = per_base.map{|c| (10**(c[2].to_f/-10)).to_f }
-          -10 * Math.log10(v.reduce(:+) / v.size)
-        end
+        get_module_matrix("Per base sequence quality", 1)
       end
 
       def per_tile_sequence_quality
-        node = @object.select{|a| a.first.first == ">>Per tile sequence quality" }.first
-        node.select{|n| n.first != ">>Per tile sequence quality" } if node
+        get_module_matrix("Per tile sequence quality", 1)
       end
 
       def per_sequence_quality_scores
-        node = @object.select{|a| a.first.first == ">>Per sequence quality scores" }.first
-        node.select{|n| n.first != ">>Per sequence quality scores" } if node
+        get_module_matrix("Per sequence quality scores", 1)
       end
 
       def per_base_sequence_content
-        node = @object.select{|a| a.first.first == ">>Per base sequence content" }.first
-        node.select{|n| n.first != ">>Per base sequence content" } if node
+        get_module_matrix("Per base sequence content", 1)
       end
 
       def per_sequence_gc_content
-        node = @object.select{|a| a.first.first == ">>Per sequence GC content" }.first
-        node.select{|n| n.first != ">>Per sequence GC content" } if node
-      end
-
-      def per_sequence_gc_content
-        node = @object.select{|a| a.first.first == ">>Per sequence GC content" }.first
-        node.select{|n| n.first != ">>Per sequence GC content" } if node
+        get_module_matrix("Per sequence GC content", 1)
       end
 
       def per_base_n_content
-        node = @object.select{|a| a.first.first == ">>Per base N content" }.first
-        node.select{|n| n.first != ">>Per base N content" } if node
-      end
-
-      ## Custom module: overall N content
-      def overall_n_content
-        per_base = self.per_base_n_content
-        if per_base
-          v = per_base.map{|c| c[1].to_f }
-          v.reduce(:+) / v.size
-        end
+        get_module_matrix("Per base N content", 1)
       end
 
       def sequence_length_distribution
-        node = @object.select{|a| a.first.first == ">>Sequence Length Distribution" }.first
-        node.select{|n| n.first != ">>Sequence Length Distribution" } if node
-      end
-
-      ## Custom module: mean sequence length calculated from distribution
-      def mean_sequence_length
-        distribution = self.sequence_length_distribution
-        if distribution
-          sum = distribution.map do |length_count|
-            length = length_count[0]
-            count = length_count[1].to_f
-            if length =~ /\d-\d/
-              f = length.sub(/-\d+$/,"").to_i
-              b = length.sub(/^\d+-/,"").to_i
-              mean = (f + b) / 2
-              mean * count
-            else
-              length.to_i * count
-            end
-          end
-          sum.reduce(:+) / self.total_sequences
-        end
-      end
-
-      ## Custom module: median sequence length calculated from distribution
-      def median_sequence_length
-        distribution = self.sequence_length_distribution
-        if distribution
-          array = distribution.map do |length_count|
-            length = length_count[0]
-            count = length_count[1].to_i
-            if length =~ /\d-\d/
-              f = length.sub(/-\d+$/,"").to_i
-              b = length.sub(/^\d+-/,"").to_i
-              mean = (f + b) / 2
-              [mean.to_f] * count
-            else
-              [length.to_f] * count
-            end
-          end
-          sorted = array.flatten.sort
-          quot = sorted.size / 2
-          if !sorted.size.even?
-            sorted[quot]
-          else
-            f = sorted[quot]
-            b = sorted[quot - 1]
-            (f + b) / 2
-          end
-        end
-      end
-
-      def sequence_duplication_levels
-        node = @object.select{|a| a.first.first == ">>Sequence Duplication Levels" }.first
-        if node
-          node.shift(3)
-          node
-        end
+        get_module_matrix("Sequence Length Distribution", 1)
       end
 
       def total_duplicate_percentage
-        node = @object.select{|a| a.first.first == ">>Sequence Duplication Levels" }.first
-        node.select{|n| n.first == "\#Total Duplicate Percentage" }.flatten[1].to_f if node
+        get_module_matrix("Sequence Duplication Levels", 1)[0][1].to_f
+      end
+
+      def sequence_duplication_levels
+        get_module_matrix("Sequence Duplication Levels", 2)
       end
 
       def overrepresented_sequences
-        node = @object.select{|a| a.first.first == ">>Overrepresented sequences" }.first
-        node.select{|n| n.first != ">>Overrepresented sequences" } if node
+        get_module_matrix("Overrepresented sequences", 1)
       end
 
       def adapter_content
-        node = @object.select{|a| a.first.first == ">>Adapter Content" }.first
-        node.select{|n| n.first != ">>Adapter Content" } if node
+        get_module_matrix("Adapter Content", 1)
       end
 
       def kmer_content
-        node = @object.select{|a| a.first.first == ">>Kmer Content" }.first
-        node.select{|n| n.first != ">>Kmer Content" } if node
+        get_module_matrix("Kmer Content", 1)
+      end
+
+      #
+      # Custom modules
+      #
+
+      def min_length
+        sequence_length.sub(/-\d+$/,"").to_i
+      end
+
+      def max_length
+        sequence_length.sub(/^\d+-/,"").to_i
+      end
+
+      def per_base_quality_column(mean_or_median)
+        case mean_or_median
+        when :mean
+          1
+        when :median
+          2
+        end
+      end
+
+      def overall_quality_score(mean_or_median)
+        per_base = per_base_sequence_quality
+        per_base.shift # drop header
+        column = per_base_quality_column(mean_or_median)
+        v = per_base.map do |row|
+          (10**(row[column].to_f / -10)).to_f
+        end
+        -10 * Math.log10(v.reduce(:+) / v.size)
+      end
+
+      def overall_mean_quality_score
+        overall_quality_score(:mean)
+      end
+
+      def overall_median_quality_score
+        overall_quality_score(:median)
+      end
+
+      def overall_n_content
+        per_base = per_base_n_content
+        v = per_base.map{|c| c[1].to_f }
+        v.reduce(:+) / v.size
+      end
+
+      def mean_sequence_length
+        dist = sequence_length_distribution
+        dist.shift # drop column header
+        if dist.size == 1
+          dist[0][0].to_f
+        else
+          sum = dist.map do |length_count|
+            l = length_count[0]
+            c  = length_count[1].to_f
+            if l =~ /\d-\d/
+              ((l.sub(/-\d+$/,"").to_f + l.sub(/^\d+-/,"").to_f) / 2 ) * c
+            else
+              l.to_i * c
+            end
+          end
+          sum.reduce(:+) / sum.size
+        end
+      end
+
+      def median_sequence_length
+        dist = sequence_length_distribution
+        dist.shift # drop column header
+        if dist.size == 1
+          dist[0][0].to_f
+        else
+          array = dist.map do |length_count|
+            l = length_count[0]
+            c = length_count[1].to_f
+            c.times.map{ l }
+          end
+          median(array.flatten)
+        end
+      end
+
+      def median(array)
+        a = array.dup
+        k = array.size / 2
+        loop do
+          pivot = a.delete_at(rand(a.size))
+          left, right = a.partition{|x| x < pivot }
+          if k == left.length
+            return pivot
+          elsif k < left.length
+            a = left
+          else
+            k = k - left.length - 1
+            a = right
+          end
+        end
       end
 
       def summary
+        parse
+      end
+
+      def parse
         {
-          fastqc_version: self.fastqc_version,
-          filename: self.filename,
-          file_type: self.file_type,
-          encoding: self.encoding,
-          total_sequences: self.total_sequences,
-          filtered_sequences: self.filtered_sequences,
-          sequence_length: self.sequence_length,
-          percent_gc: self.percent_gc,
-          per_base_sequence_quality: self.per_base_sequence_quality,
-          per_tile_sequence_quality: self.per_tile_sequence_quality,
-          per_sequence_quality_scores: self.per_sequence_quality_scores,
-          per_base_sequence_content: self.per_base_sequence_content,
-          per_sequence_gc_content: self.per_sequence_gc_content,
-          per_base_n_content: self.per_base_n_content,
-          sequence_length_distribution: self.sequence_length_distribution,
-          total_duplicate_percentage: self.total_duplicate_percentage,
-          sequence_duplication_levels: self.sequence_duplication_levels,
-          overrepresented_sequences: self.overrepresented_sequences,
-          adapter_content: self.adapter_content,
-          kmer_content: self.kmer_content,
-          min_length: self.min_length,
-          max_length: self.max_length,
-          overall_mean_quality_score: self.overall_mean_quality_score,
-          overall_median_quality_score: self.overall_median_quality_score,
-          overall_n_content: self.overall_n_content,
-          mean_sequence_length: self.mean_sequence_length,
-          median_sequence_length: self.median_sequence_length,
+          fastqc_version: fastqc_version,
+          filename: filename,
+          file_type: file_type,
+          encoding: encoding,
+          total_sequences: total_sequences,
+          sequences_flagged_as_poor_quality: sequences_flagged_as_poor_quality,
+          filtered_sequences: filtered_sequences,
+          sequence_length: sequence_length,
+          percent_gc: percent_gc,
+          per_base_sequence_quality: per_base_sequence_quality,
+          per_tile_sequence_quality: per_tile_sequence_quality,
+          per_sequence_quality_scores: per_sequence_quality_scores,
+          per_base_sequence_content: per_base_sequence_content,
+          per_sequence_gc_content: per_sequence_gc_content,
+          per_base_n_content: per_base_n_content,
+          sequence_length_distribution: sequence_length_distribution,
+          total_duplicate_percentage: total_duplicate_percentage,
+          sequence_duplication_levels: sequence_duplication_levels,
+          overrepresented_sequences: overrepresented_sequences,
+          adapter_content: adapter_content,
+          kmer_content: kmer_content,
+          min_length: min_length,
+          max_length: max_length,
+          overall_mean_quality_score: overall_mean_quality_score,
+          overall_median_quality_score: overall_median_quality_score,
+          overall_n_content: overall_n_content,
+          mean_sequence_length: mean_sequence_length,
+          median_sequence_length: median_sequence_length,
         }
       end
     end
