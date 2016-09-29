68e9a8a23bb04dfb38431eca1d2fcdd9a6c04a6c
Tazro Inutano Ohta <inutano@gmail.com>
2016-09-29 14:30:55 +0900

    tab to space

--- lib/bio/fastqc/data.rb
+++ lib/bio/fastqc/data.rb
@@ -5,37 +5,37 @@ require 'zip'
 module Bio
   module FastQC
     class Data
-  		class << self
-  			def read(file)
-  				read_zipfile(file)
-  			rescue Zip::Error
-  				read_flatfile(file)
+      class << self
+        def read(file)
+          read_zipfile(file)
+        rescue Zip::Error
+          read_flatfile(file)
         rescue Errno::EISDIR
           read_dir(file)
-  			end
+        end
 
-  			def read_zipfile(file)
-  				Zip::File.open(file) do |zipfile|
-  					d = zipfile.glob('*/fastqc_data.txt').first
+        def read_zipfile(file)
+          Zip::File.open(file) do |zipfile|
+            d = zipfile.glob('*/fastqc_data.txt').first
             filenotfound(file) if !d
             d.get_input_stream.read
-  				end
-  			end
+          end
+        end
 
-  			def read_flatfile(file)
-  				open(file).read
-  			end
+        def read_flatfile(file)
+          open(file).read
+        end
 
-  			def read_dir(file)
-  				open(File.join(file, "fastqc_data.txt")).read
-  			rescue Errno::ENOENT
+        def read_dir(file)
+          open(File.join(file, "fastqc_data.txt")).read
+        rescue Errno::ENOENT
           filenotfound(file)
-  			end
+        end
 
         def filenotfound(file)
           raise "FastQC data file fastqc_data.txt not found, input file: #{file}"
         end
-  		end
-  	end
+      end
+    end
   end
 end
