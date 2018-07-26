35bcedf3f9dec43778376654caf1ade4ed5aa496
Tazro Inutano Ohta <inutano@gmail.com>
2018-07-26 14:03:38 +0900

    accept multiple input files

--- lib/bio/fastqc/cli.rb
+++ lib/bio/fastqc/cli.rb
@@ -8,12 +8,12 @@ module Bio
     class CLI < Thor
       desc "parse [--format format] [filename]", "parse fastqc data in directory or zipfile. output format should be json, json-ld, turtle, or tsv"
       option :format, :default => "json"
-      def parse(file)
-        data = Data.read(file)
-        summary = Parser.new(data).summary
-        puts Converter.new(summary).convert_to(options[:format])
-      # rescue
-      #   puts "Wrong input file type: specify fastqc result data, directory or zipfile"
+      def parse(*files)
+        files.each do |file|
+          data = Data.read(file)
+          summary = Parser.new(data).summary
+          puts Converter.new(summary).convert_to(options[:format])
+        end
       end
     end
   end
