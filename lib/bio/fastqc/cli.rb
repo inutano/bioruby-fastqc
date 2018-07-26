9d2dfc9a4a3fe056e4f38229036affb2ce82c48d
Tazro Inutano Ohta <inutano@gmail.com>
2018-07-26 14:32:35 +0900

    added outdir option to CLI

--- lib/bio/fastqc/cli.rb
+++ lib/bio/fastqc/cli.rb
@@ -6,13 +6,23 @@ require 'json'
 module Bio
   module FastQC
     class CLI < Thor
-      desc "parse [--format format] [filename]", "parse fastqc data in directory or zipfile. output format should be json, json-ld, turtle, or tsv"
+      desc "parse [--format format] [--outdir output directory] [filepath, ..]", "parse fastqc data in directory or zipfile. output format: json, json-ld, turtle, or tsv"
       option :format, :default => "json"
+      option :output, :default => nil
       def parse(*files)
         files.each do |file|
           data = Data.read(file)
           summary = Parser.new(data).summary
-          puts Converter.new(summary).convert_to(options[:format])
+          out = Converter.new(summary).convert_to(options[:format])
+
+          outdir_path = options[:outdir]
+          if outdir_path && File.directory?(outdir_path)
+            filename_org = File.basename(summary[:filename])
+            fpath = File.join(outdir_path, filename_org + "." + options[:format])
+            open(fpath, "w"){|f| f.puts(out) }
+          else
+            puts out
+          end
         end
       end
     end
